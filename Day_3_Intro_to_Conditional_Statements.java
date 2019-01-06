62222ce4464e941dc0ad863abd7336aab5b20e45
Prabhat_geeks <46269497+geeksPrabhat@users.noreply.github.com>
2019-01-07 02:17:29 +0530

    Add files via upload

---
+++ Day_3_Intro_to_Conditional_Statements.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+ 
+public class Day_3_Intro_to_Conditional_Statements
+{ 
+    public static void main(String[] args)
+    {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        scan.close();
+        if (n % 2 == 0)
+        {
+            if (n < 6) 
+                System.out.println("Not Weird");
+            else if(n < 21) 
+                System.out.println("Weird");
+            else 
+                System.out.println("Not Weird");
+        }
+        else 
+            System.out.println("Weird");
+    } 
+}
