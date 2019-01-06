e97f799bef256d419029edc745a8445352dbd334
Prabhat_geeks <46269497+geeksPrabhat@users.noreply.github.com>
2019-01-07 02:24:58 +0530

    Add files via upload

---
+++ Day_4_Class_vs_Instance_Person.java
@@ -0,0 +1,58 @@
+import java.io.*;
+import java.util.*;
+
+public class Day_4_Class_vs_Instance_Person 
+{
+    private int age;    
+    public Day_4_Class_vs_Instance_Person(int initialAge)
+	{
+		if(initialAge < 0)
+		{
+            age = 0;
+            System.out.println("Age is not valid, setting age to 0.");
+        }
+		else
+		{
+            age = initialAge;
+        }
+    }
+	public void amIOld()
+	{
+        String ans;
+          if(age < 13)
+		  {
+			  ans = "You are young.";
+          }
+		  else if(age >= 13 && age < 18)
+		  {
+            ans = "You are a teenager.";
+          }
+		  else
+		  {
+            ans = "You are old.";
+         }
+         System.out.println(ans);
+    }
+    public void yearPasses()
+	{
+          age += 1;
+    }
+    public static void main(String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		for (int i = 0; i < T; i++)
+		{
+			int age = sc.nextInt();
+			Day_4_Class_vs_Instance_Person p = new Day_4_Class_vs_Instance_Person(age);
+			p.amIOld();
+			for (int j = 0; j < 3; j++)
+			{
+				p.yearPasses();
+			}
+			p.amIOld();
+			System.out.println();
+        }
+		sc.close();
+    }
+}
